7e430decf6cb2c5c8c51c7b7cbad8a3845f405e5
shameless3 <653566138@qq.com>
2021-01-23 21:55:37 +0800

    modify ycsb.cc

--- tests/ycsb.cc
+++ tests/ycsb.cc
@@ -34,8 +34,8 @@ const char *workloads[] = {
   // "workloada_insert_50.spec",
   // "workloada_insert_80.spec",
   // "workloada_insert_100.spec",
-  "workload_read.spec",
-  "workload_insert.spec",
+  // "workload_read.spec",
+  "workload_test3.spec",
 
 };
 
