fb71ae07e402781cf2b3357e386edb346606b438
shameless3 <653566138@qq.com>
2021-01-24 18:09:12 +0800

    test

--- test.sh
+++ test.sh
@@ -8,18 +8,19 @@ WorkLoadsDir="./include/ycsb/workloads"
 ExecDir="./build"
 DBName="fastfair"
 RecordCount="10000000"
-writerates="0 0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9"
-writerate="0"
-readrate="1.0"
+
+loop = "0 1 2 3 4 5 6 7 8 9"
+index = "0"
+writerates="0 0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9 1.0"
+readrates="1.0 0.9 0.8 0.7 0.6 0.5 0.4 0.3 0.2 0.1 0"
 
 function OperateDiffDB() {
     RecordCount="100000000"
     sed -r -i "s/recordcount=.*/recordcount=$RecordCount/1" ${WorkLoadsDir}/workload_test3.spec
-    for writerate in $writerates;
+    for index  in $loop;
     do
-        let readrate = 1 - $writerate
-        sed -r -i "s/insertproportiont=.*/insertproportion=$writerate/1" ${WorkLoadsDir}/workload_test3.spec
-        sed -r -i "s/readproportion=.*/readproportion=$readrate/1" ${WorkLoadsDir}/workload_test3.spec
+        sed -r -i "s/insertproportiont=.*/insertproportion=${writerates[index]}/1" ${WorkLoadsDir}/workload_test3.spec
+        sed -r -i "s/readproportion=.*/readproportion=${readrates[index]}/1" ${WorkLoadsDir}/workload_test3.spec
     done
 }
 
